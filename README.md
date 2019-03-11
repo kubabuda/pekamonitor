dd8b27985b37a18272c17579c6d512166b696cdc
Kuba Buda <kubabuda@gazeta.pl>
2019-03-12 00:23:48 +0100

    Removed dead code from main.cpp, updated readme

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
-# TODO
+# VM API 
+##Request
+Symbol parameter is code for bus/tram stop (streed name + direction):
+```bash
+    curl -d "method=getTimes&p0={'symbol':'RKAP71'}" \
+         -H "Content-Type: application/x-www-form-urlencoded" \
+        -X POST https://www.peka.poznan.pl/vm/method.vm
+```
 
-POST example throws HTTPC_ERROR_READ_TIMEOUT, to debug
+Sample response (no incoming bus/trams):
+```json
+{"success":{"bollard":{"symbol":"SWRZ01","tag":"SWRZ01","name":"Świerzawska","mainBollard":false},"times":[]}}
+```
+
+Sample response with incoming lines:
+```json
+{"success":{"bollard":{"symbol":"RKAP71","tag":"RKAP01","name":"Rondo Kaponiera","mainBollard":false},
+"times":[{"realTime":false,"minutes":13,"direction":"Rondo Kaponiera","onStopPoint":false,"departure":"2019-03-12T00:21:00.000Z","line":"249"},{"realTime":true,"minutes":16,"direction":"Rondo Kaponiera","onStopPoint":false,"departure":"2019-03-12T00:24:00.000Z","line":"232"},{"realTime":true,"minutes":17,"direction":"Rondo Kaponiera","onStopPoint":false,"departure":"2019-03-12T00:25:00.000Z","line":"238"},{"realTime":false,"minutes":22,"direction":"Szwajcarska Szpital","onStopPoint":false,"departure":"2019-03-12T00:30:00.000Z","line":"232"},{"realTime":false,"minutes":22,"direction":"Szwajcarska Szpital","onStopPoint":false,"departure":"2019-03-12T00:30:00.000Z","line":"238"},{"realTime":false,"minutes":22,"direction":"Dębiec","onStopPoint":false,"departure":"2019-03-12T00:30:00.000Z","line":"249"}]}}
+```
+
+# Cert fingertip
+Getting fingerprint:
+```bash
+        echo | openssl s_client -connect peka.poznan.pl:443 | openssl x509 -fingerprint -noout
+```
+As for writing it:      
+```  
+BA:ED:B9:EB:E4:46:D3:16:49:40:34:DC:88:66:76:81:28:68:8B:1D
+```
+Format required by BearSSL::WiFiClientSecure is
+```c++
+const uint8_t fingerprint[20] =  { 0xBA, 0xED, 0xB9, 0xEB, 0xE4, 0x46, 0xD3, 0x16, 0x49, 0x40, 0x34, 0xDC, 0x88, 0x66, 0x76, 0x81, 0x28, 0x68, 0x8B, 0x1D };
+```
+
+# TODO
+Store secrets (SSID, password) outside version control (and cleanup git history)
+Parametrize request with selected 
+Parse answer with arduino json
+https://arduinojson.org/
+Display with SparkFun Micro OLED
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,9 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecureBearSSL.h>
 
-const char* host = "https://www.peka.poznan.pl/vm/";
 const uint8_t fingerprint[20] =  { 0xBA, 0xED, 0xB9, 0xEB, 0xE4, 0x46, 0xD3, 0x16, 0x49, 0x40, 0x34, 0xDC, 0x88, 0x66, 0x76, 0x81, 0x28, 0x68, 0x8B, 0x1D };
 const char* postEndpoint = "https://www.peka.poznan.pl/vm/method.vm";
-const char* postPayload = "method=getTimes&p0={'symbol':'SWRZ01'}";
+const char* payload = "method=getTimes&p0={'symbol':'SWRZ01'}";
 
 ESP8266WiFiMulti WiFiMulti;
 const char* ssid = "***REMOVED***";
@@ -16,23 +15,10 @@ const char* password = "***REMOVED***";
 const int buttonPin = D7;
 volatile bool wasPressed = false;
 
-
 // struct bus_stop {
 //   String symbol;
 //   String name;
 // } typedef bus_stop;
-
-/* VM API works like that:
-curl -d "method=getTimes&p0={'symbol':'SWRZ01'}" \
-     -H "Content-Type: application/x-www-form-urlencoded" \
-     -X POST https://www.peka.poznan.pl/vm/method.vm
-
-  Getting fingerprint:
-    echo | openssl s_client -connect peka.poznan.pl:443 | openssl x509 -fingerprint -noout
-
-  BA:ED:B9:EB:E4:46:D3:16:49:40:34:DC:88:66:76:81:28:68:8B:1D
-
- */
 
 // bus_stop stops[] = {
 //   { .symbol = "SWRZ01", .name = "Świerzawska->Ławica" },
@@ -41,12 +27,10 @@ curl -d "method=getTimes&p0={'symbol':'SWRZ01'}" \
 // };
 // int stopsCount = 2;
 // volatile int currentStop = 0;
-// String postParams = "method=getTimes&p0={'symbol':'BIAL02'}";
 
 
-
+// methods declarations
 void handleKeyPress();
-int httpsGET(const char *host, const char *fingerprint, String url);
 void connect();
 
 
@@ -100,7 +84,7 @@ void connect() {
 
       Serial.print("[HTTPS] POST...\n");
       // start connection and send HTTP header
-      int httpCode = https.POST(postPayload);
+      int httpCode = https.POST(payload);
 
       // httpCode will be negative on error
       if (httpCode > 0) {
@@ -114,44 +98,6 @@ void connect() {
         }
       } else {
         Serial.printf("[HTTPS] POST... failed, error: %s [%d]\n", https.errorToString(httpCode).c_str(), httpCode);
-      }
-
-      https.end();
-    } else {
-      Serial.printf("[HTTPS] Unable to connect\n");
-    }
-  }
-}
-
-
-void connect_get() {
-  if ((WiFiMulti.run() == WL_CONNECTED)) {
-
-    std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
-
-    client->setFingerprint(fingerprint);
-
-    HTTPClient https;
-
-    Serial.print("[HTTPS] begin...\n");
-    if (https.begin(*client, host)) {  // HTTPS
-
-      Serial.print("[HTTPS] GET...\n");
-      // start connection and send HTTP header
-      int httpCode = https.GET();
-      
-      // httpCode will be negative on error
-      if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
-
-        // file found at server
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          String payload = https.getString();
-          Serial.println(payload);
-        }
-      } else {
-        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
       }
 
       https.end();
