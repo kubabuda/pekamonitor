a08bbf7446484dcb5379aba25afb668eb95a55af
Kuba Buda <kubabuda@gazeta.pl>
2019-03-12 23:53:18 +0100

    debounce works, but external int has higher priority and can mask symbol display

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ StaticJsonDocument<MAX_RESPONSE_SIZE> response;
 
 const int buttonPin = D7;
 volatile bool wasPressed = false;
+volatile bool wasLoaded = false;
 
 
 // methods declarations
@@ -41,27 +42,30 @@ void setup()
 
 void loop()
 {
+  String symbol = bollards[currentBollard].symbol;
   if (wasPressed) {
     if ((WiFiMulti.run() == WL_CONNECTED)) {
-      String symbol = bollards[currentBollard].symbol;
       displaySymbol(symbol);
-      connect(symbol, &response); // TODO to state machine to avoid wtd rst
-      // displayResponse(&response);
+      connect(symbol, &response);
     } else {
       Serial.printf("[WARN] Request omited, wifi not connected\n");
     } 
-    wasPressed = false;
+    wasPressed = false; // TODO state machine to avoid wtd rst
+    wasLoaded = true;   // probably theres framework for this
+  } else if(wasLoaded) {
+    // displayResponse(&response); // watchdog still resets on this
+    wasLoaded = false;
   }
 }
-
-
 
 
 void handleKeyPress() {
   static volatile uint32_t prev = 0;
   auto debounce_ms = 1000;
-  auto now = millis();
+  auto now = millis();   // TODO there must be lib for this
   if(now > prev + debounce_ms) {
+    Serial.printf("[DEBUG] prev %ul, now %ul, boll %ul\n", prev, now, currentBollard);
+    prev = now;
     currentBollard = (currentBollard + 1) % bollardsCount;
     wasPressed = true;
   }
